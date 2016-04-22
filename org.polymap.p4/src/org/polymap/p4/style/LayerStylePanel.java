48d9f1e63d66e098c4c01b689509a50d07928e87
Steffen Stundzig <git@stundzig.de>
2016-04-22 11:52:47 +0200

    41: [Style] PolygonStyle
    Task-Url: http://github.com/Polymap4/polymap4-core/issues/issue/41
    
    support multi styles

--- org.polymap.p4/src/org/polymap/p4/style/LayerStylePanel.java
+++ org.polymap.p4/src/org/polymap/p4/style/LayerStylePanel.java
@@ -1,20 +1,16 @@
-/* 
- * polymap.org
- * Copyright (C) 2015, Falko Bräutigam. All rights reserved.
+/*
+ * polymap.org Copyright (C) 2015, Falko Bräutigam. All rights reserved.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3.0 of the License, or (at your option) any later
+ * version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  */
 package org.polymap.p4.style;
-
-import static com.google.common.collect.Iterables.getOnlyElement;
 
 import java.util.Collection;
 
@@ -27,6 +23,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.polymap.core.project.ILayer;
 import org.polymap.core.style.model.FeatureStyle;
+import org.polymap.core.style.model.PointStyle;
 import org.polymap.core.style.model.Style;
 import org.polymap.core.style.model.StylePropertyValue;
 import org.polymap.core.style.ui.StylePropertyField;
@@ -53,13 +50,13 @@ import org.polymap.p4.layer.LayerInfoPanel;
 public class LayerStylePanel
         extends P4Panel {
 
-    private static Log log = LogFactory.getLog( LayerStylePanel.class );
+    private static Log                  log = LogFactory.getLog( LayerStylePanel.class );
 
-    public static final PanelIdentifier ID = PanelIdentifier.parse( "layerStyle" );
-    
+    public static final PanelIdentifier ID  = PanelIdentifier.parse( "layerStyle" );
+
     private FeatureStyle                featureStyle;
-    
-    
+
+
     @Override
     public boolean beforeInit() {
         IPanel parent = getContext().getPanel( getSite().getPath().removeLast( 1 ) );
@@ -78,7 +75,7 @@ public class LayerStylePanel
         try {
             ILayer layer = featureSelection.get().layer();
             featureStyle = P4Plugin.styleRepo().featureStyle( layer.styleIdentifier.get() )
-                    .orElseThrow( () -> new IllegalStateException( "Layer has no style.") );
+                    .orElseThrow( () -> new IllegalStateException( "Layer has no style." ) );
         }
         catch (Exception e) {
             StatusDispatcher.handleError( "Unable to get style of layer.", e );
@@ -88,36 +85,41 @@ public class LayerStylePanel
 
     @Override
     public void createContents( Composite parent ) {
-        getSite().setTitle( "Styling" );  // + ": " + layer.get().label.get() );
+        getSite().setTitle( "Styling" ); // + ": " + layer.get().label.get() );
         getSite().setPreferredWidth( 350 );
         ContributionManager.instance().contributeTo( this, this );
-        
-        IPanelSection editorSection = tk().createPanelSection( parent, "Point"/*, SWT.BORDER*/ );
-        createStyleEditor( editorSection.getBody(), getOnlyElement( featureStyle.members() ) );
-        
+
+        for (Style style : featureStyle.members()) {
+            IPanelSection editorSection = tk().createPanelSection( parent,
+                    PointStyle.class.isInstance( style ) ? "Point" : "Polygon" );
+            createStyleEditor( editorSection.getBody(), style );
+        }
+
         // fab
         Button fab = tk().createFab();
         fab.setToolTipText( "Save changes" );
         fab.addSelectionListener( new SelectionAdapter() {
+
             @Override
             public void widgetSelected( SelectionEvent ev ) {
                 featureStyle.store();
                 tk().createSnackbar( Appearance.FadeIn, "Saved" );
             }
-        });
+        } );
     }
-    
-    
+
+
     protected void createStyleEditor( Composite parent, Style style ) {
         parent.setLayout( ColumnLayoutFactory.defaults().columns( 1, 1 ).margins( 0, 5 ).spacing( 5 ).create() );
-        
+
         Collection<PropertyInfo<StylePropertyValue>> propInfos = style.info().getProperties();
         for (PropertyInfo<StylePropertyValue> propInfo : propInfos) {
             if (StylePropertyValue.class.isAssignableFrom( propInfo.getType() )) {
-                StylePropertyField field = new StylePropertyField( (Property<StylePropertyValue>)propInfo.get( style ) );
+                StylePropertyField field = new StylePropertyField(
+                        (Property<StylePropertyValue>)propInfo.get( style ) );
                 field.createContents( parent );
             }
         }
     }
-    
+
 }
