779ef5f5a42ee5e2a64748a4144b0dc72422ed61
Steffen Stundzig <kontakt@stundzig.de>
2016-06-20 13:23:16 +0200

    54: [Import] WMS: refresh of the preview
    Task-Url: http://github.com/Polymap4/polymap4-p4/issues/issue/54
    
      - add abstract
      - open needs scrollbars

--- org.polymap.p4.data.importer/src/org/polymap/p4/data/importer/wms/WmsImporter.java
+++ org.polymap.p4.data.importer/src/org/polymap/p4/data/importer/wms/WmsImporter.java
@@ -42,7 +42,6 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-
 import org.polymap.core.catalog.IUpdateableMetadataCatalog.Updater;
 import org.polymap.core.data.wms.catalog.WmsServiceResolver;
 import org.polymap.core.runtime.UIThreadExecutor;
@@ -152,7 +151,10 @@ public class WmsImporter
                 String title = capabilities.getService().getTitle();
                 log.info( "Service title: " + title );
 
-                if (P4Plugin.localCatalog().query( "" ).execute().stream().filter( metadata -> metadata.getTitle().equals( title ) ).findAny().isPresent()) {
+                if (P4Plugin.localCatalog().query( "" ).execute().stream()
+                        .filter( metadata -> metadata.getTitle().equals( title ) )
+                        .findAny()
+                        .isPresent()) {
                     throw new Exception( i18nWMS.get( "entryExists", title ) );
                 }
                 site.ok.set( true );
@@ -169,82 +171,97 @@ public class WmsImporter
     @Override
     public void createResultViewer( Composite parent, IPanelToolkit tk ) {
         this.toolkit = tk;
+//        final ScrolledComposite scrolledComposite = new ScrolledComposite( parent, SWT.V_SCROLL | SWT.H_SCROLL );
+//        scrolledComposite.setExpandHorizontal( true );
+//        scrolledComposite.setExpandVertical( true );
+//
+//        final Composite content = new Composite( scrolledComposite, SWT.NONE );
+//        scrolledComposite.setContent( content );
+        final Composite content = parent;
+        
         if (exception != null) {
-            //StatusDispatcher.handleError( i18nWMS.get( "exception", exception.getMessage() ), exception );
-            tk.createFlowText( parent, i18nWMS.get( "exception", exception.getMessage() ) );
+            tk.createFlowText( content, i18nWMS.get( "exception", exception.getMessage() ) );
         }
         else if (wms == null) {
-            tk.createFlowText( parent, i18nWMS.get( "noUrl" ) );
+            tk.createFlowText( content, i18nWMS.get( "noUrl" ) );
         }
         else {
             StringBuilder msg = new StringBuilder();
             WMSCapabilities capabilities = wms.getCapabilities();
             Service serviceInfo = capabilities.getService();
             msg.append( "### " + serviceInfo.getTitle() + " (" + serviceInfo.getName() + ")\n\n" );
+            if (!StringUtils.isBlank( serviceInfo.get_abstract() )) {
+                msg.append( serviceInfo.get_abstract() ).append( "\n\n" );
+            }
+            if (serviceInfo.getOnlineResource()  != null) {
+                msg.append( "### " ).append( i18nWMS.get( "onlineResource" ) ).append( "\n" );
+                msg.append( serviceInfo.getOnlineResource() ).append( "\n" );
+                
+            }
             ResponsibleParty contactInformation = serviceInfo.getContactInformation();
             if (contactInformation != null) {
-                msg.append( "### " ).append( i18nWMS.get( "contactInfo" ) ).append( ":\n" );
+                msg.append( "### " ).append( i18nWMS.get( "contactInfo" ) ).append( "\n" );
                 if (!StringUtils.isBlank( contactInformation.getOrganisationName() )) {
-                    msg.append( "  * " ).append( i18nWMS.get( "organisationName" ) ).append( ": " ).append( contactInformation.getOrganisationName() ).append("\n");
+                    msg.append( "  * " ).append( i18nWMS.get( "organisationName" ) ).append( ": " ).append( contactInformation.getOrganisationName() ).append( "\n" );
                 }
                 if (!StringUtils.isBlank( contactInformation.getIndividualName() )) {
-                    msg.append( "  * " ).append( i18nWMS.get( "individualName" ) ).append( ": " ).append( contactInformation.getIndividualName() ).append("\n");
+                    msg.append( "  * " ).append( i18nWMS.get( "individualName" ) ).append( ": " ).append( contactInformation.getIndividualName() ).append( "\n" );
                 }
                 if (!StringUtils.isBlank( contactInformation.getPositionName() )) {
-                    msg.append( "  * " ).append( i18nWMS.get( "positionName" ) ).append( ": " ).append( contactInformation.getPositionName() ).append("\n");
+                    msg.append( "  * " ).append( i18nWMS.get( "positionName" ) ).append( ": " ).append( contactInformation.getPositionName() ).append( "\n" );
                 }
                 msg.append( "\n" );
                 Contact contactInfo = contactInformation.getContactInfo();
                 if (contactInfo != null) {
                     contactInfo.getContactInstructions();
                     contactInfo.getHoursOfService();
-//                    if (!StringUtils.isBlank( contactInfo.getPhone().Address().getPositionName() )) {
-//                        msg.append( "### " ).append( i18nWMS.get( "positionName" ) ).append( ": " ).append( contactInformation.getPositionName() );
-//                    }
                     Address address = contactInfo.getAddress();
                     if (address != null) {
-                        msg.append( "### " ).append( i18nWMS.get( "address" ) ).append( ":\n" );
+                        msg.append( "### " ).append( i18nWMS.get( "address" ) ).append( "\n" );
                         if (!StringUtils.isBlank( address.getCountry() )) {
-                            msg.append( "  * " ).append( i18nWMS.get( "country" ) ).append( ": " ).append( address.getCountry() ).append("\n");
+                            msg.append( "  * " ).append( i18nWMS.get( "country" ) ).append( ": " ).append( address.getCountry() ).append( "\n" );
                         }
                         if (!StringUtils.isBlank( address.getAdministrativeArea() )) {
-                            msg.append( "  * " ).append( i18nWMS.get( "administrativeArea" ) ).append( ": " ).append( address.getAdministrativeArea()  ).append("\n");
+                            msg.append( "  * " ).append( i18nWMS.get( "administrativeArea" ) ).append( ": " ).append( address.getAdministrativeArea() ).append( "\n" );
                         }
                         if (!StringUtils.isBlank( address.getCity() )) {
-                            msg.append( "  * " ).append( i18nWMS.get( "city" ) ).append( ": " ).append( address.getCity()  ).append("\n");
+                            msg.append( "  * " ).append( i18nWMS.get( "city" ) ).append( ": " ).append( address.getCity() ).append( "\n" );
                         }
                         if (!StringUtils.isBlank( address.getPostalCode() )) {
-                            msg.append( "  * " ).append( i18nWMS.get( "postalCode" ) ).append( ": " ).append( address.getPostalCode()  ).append("\n");
+                            msg.append( "  * " ).append( i18nWMS.get( "postalCode" ) ).append( ": " ).append( address.getPostalCode() ).append( "\n" );
                         }
-                        if ( address.getDeliveryPoints() != null && !address.getDeliveryPoints().isEmpty()) {
-                            msg.append( "  * " ).append( i18nWMS.get( "deliveryPoints" ) ).append( ": " ).append( address.getDeliveryPoints().stream().collect(Collectors.joining(", "))  ).append("\n");
+                        if (address.getDeliveryPoints() != null && !address.getDeliveryPoints().isEmpty()) {
+                            msg.append( "  * " ).append( i18nWMS.get( "deliveryPoints" ) ).append( ": " ).append( address.getDeliveryPoints().stream().collect( Collectors.joining( ", " ) ) ).append( "\n" );
                         }
-                        if ( address.getElectronicMailAddresses() != null && !address.getElectronicMailAddresses().isEmpty()) {
-                            msg.append( "  * " ).append( i18nWMS.get( "emails" ) ).append( ": " ).append( address.getElectronicMailAddresses().stream().collect(Collectors.joining(", "))  ).append("\n");
+                        if (address.getElectronicMailAddresses() != null
+                                && !address.getElectronicMailAddresses().isEmpty()) {
+                            msg.append( "  * " ).append( i18nWMS.get( "emails" ) ).append( ": " ).append( address.getElectronicMailAddresses().stream().collect( Collectors.joining( ", " ) ) ).append( "\n" );
                         }
                     }
                     Telephone phone = contactInfo.getPhone();
                     if (phone != null) {
-                        if ( phone.getVoices() != null && !phone.getVoices().isEmpty()) {
-                            msg.append( "  * " ).append( i18nWMS.get( "phoneVoices" ) ).append( ": " ).append( phone.getVoices().stream().collect(Collectors.joining(", "))  ).append("\n");
+                        if (phone.getVoices() != null && !phone.getVoices().isEmpty()) {
+                            msg.append( "  * " ).append( i18nWMS.get( "phoneVoices" ) ).append( ": " ).append( phone.getVoices().stream().collect( Collectors.joining( ", " ) ) ).append( "\n" );
                         }
-                        if ( phone.getFacsimiles() != null && !phone.getFacsimiles().isEmpty()) {
-                            msg.append( "  * " ).append( i18nWMS.get( "phoneFacsimiles" ) ).append( ": " ).append( phone.getFacsimiles().stream().collect(Collectors.joining(", "))  ).append("\n");
+                        if (phone.getFacsimiles() != null && !phone.getFacsimiles().isEmpty()) {
+                            msg.append( "  * " ).append( i18nWMS.get( "phoneFacsimiles" ) ).append( ": " ).append( phone.getFacsimiles().stream().collect( Collectors.joining( ", " ) ) ).append( "\n" );
                         }
                     }
-                    
-                    if (!StringUtils.isBlank( contactInfo.getHoursOfService())) {
-                        msg.append( "  * " ).append( i18nWMS.get( "hoursOfService" ) ).append( ": " ).append( contactInfo.getHoursOfService() ).append("\n");
+
+                    if (!StringUtils.isBlank( contactInfo.getHoursOfService() )) {
+                        msg.append( "  * " ).append( i18nWMS.get( "hoursOfService" ) ).append( ": " ).append( contactInfo.getHoursOfService() ).append( "\n" );
                     }
                     msg.append( "\n" );
                 }
             }
-            msg.append( "### " ).append( i18nWMS.get( "layers" ) ).append( ":\n" );
+            msg.append( "### " ).append( i18nWMS.get( "layers" ) ).append( "\n" );
             for (Layer layer : capabilities.getLayerList()) {
                 msg.append( "  * " + layer.getTitle() + "\n" );
             }
             msg.append( "\n" );
-            tk.createFlowText( parent, msg.toString() );
+            tk.createFlowText( content, msg.toString() );
+//            content.layout( true );
+//            scrolledComposite.setMinSize( content.computeSize( SWT.DEFAULT, SWT.DEFAULT ) );
         }
     }
 
