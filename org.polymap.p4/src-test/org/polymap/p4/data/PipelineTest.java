e1f753df91e54d09d9d6a8c22f46717644c84646
Steffen Stundzig <git@stundzig.de>
2016-08-17 10:14:41 +0200

    63: [GeoServer] multi-layer GetMap request
    Task-Url: http://github.com/Polymap4/polymap4-core/issues/issue/63
    
      - add another test which shows EncodedImageProducer is working well

--- org.polymap.p4/src-test/org/polymap/p4/data/PipelineTest.java
+++ org.polymap.p4/src-test/org/polymap/p4/data/PipelineTest.java
@@ -16,8 +16,14 @@ package org.polymap.p4.data;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+
+import org.geotools.data.ows.Layer;
+import org.geotools.data.ows.WMSCapabilities;
 import org.geotools.data.wms.WebMapServer;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +33,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.google.common.collect.Lists;
+
+import org.polymap.core.data.image.EncodedImageProducer;
 import org.polymap.core.data.image.ImageProducer;
 import org.polymap.core.data.pipeline.DataSourceDescription;
 import org.polymap.core.data.pipeline.Pipeline;
@@ -38,6 +47,23 @@ public class PipelineTest {
     
     @Mock
     private WebMapServer webMapServer;
+
+    @Mock
+    private WMSCapabilities capabilities;
+    
+    private List<Layer> layerList;
+    
+    @Before
+    public void setup() {
+        layerList = Lists.newArrayList();
+        Layer wms = new Layer( "WMS" );
+        wms.setName( "WMS" );
+        layerList.add( wms );
+        
+        when(webMapServer.getCapabilities()).thenReturn(capabilities);
+        when( capabilities.getLayerList() ).thenReturn( layerList );
+    }
+    
     
     @Test
     @Ignore
@@ -52,5 +78,20 @@ public class PipelineTest {
         Pipeline p = incubator.newPipeline( ImageProducer.class, dsd, null );
         assertNotNull( p );
         assertTrue( "no processors in pipeline", p.length() > 0);
+    }
+    
+    
+    @Test
+    public void checkWMS2EncodedImage() throws Exception {
+        
+        P4PipelineIncubator incubator = P4PipelineIncubator.forLayer( null );
+        
+        DataSourceDescription dsd = new DataSourceDescription();
+        dsd.service.set( webMapServer );
+        dsd.resourceName.set( "WMS" );
+        
+        Pipeline p = incubator.newPipeline( EncodedImageProducer.class, dsd, null );
+        assertNotNull( p );
+        assertTrue( "one processors must be in pipeline", p.length() == 1);
     }
 }
