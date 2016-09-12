1552d9fda42f3dab097bbcd0eeadbf3fd4abea7a
Falko Bräutigam <falko@polymap.de>
2016-09-12 22:31:08 +0200

    #85: [Data] Pipeline: native getBounds() (http://github.com/Polymap4/polymap4-core/issues/issue/85)
      - another try for tiled rendering

--- org.polymap.p4/src/org/polymap/p4/map/ProjectLayerProvider.java
+++ org.polymap.p4/src/org/polymap/p4/map/ProjectLayerProvider.java
@@ -125,7 +125,7 @@ public class ProjectLayerProvider
     public Layer getLayer( ILayer elm ) {
         // start creating pipeline
         String layerName = createPipeline( elm );
-        return buildLayer( layerName );
+        return buildTiledLayer( layerName );
     }
     
 
